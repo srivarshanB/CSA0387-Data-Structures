448bd1c726aec0a8b43e6106b7fda37ff70ed762
srivarshanB <112372900+srivarshanB@users.noreply.github.com>
2023-08-28 10:15:05 +0530

    Add files via upload

---
+++ DAY-2/11.Linear search.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int ls(int arr[], int size, int target) {
+	int i;
+    for (i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  
+        }
+    }
+    return -1;  
+}
+int main() {
+    int arr[] = {10, 25, 3, 42, 17, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 17;    
+    int index = ls(arr, size, target);
+    if (index != -1) {
+        printf("Element %d found at index %d.\n", target, index);
+    } else {
+        printf("Element %d not found in the array.\n", target);
+    }
+     return 0;
+}
+
